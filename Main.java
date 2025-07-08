4733e3caa88eb91c48b239b282a7695677309c06
E-mohamed965 <mohamedkilaney2@gmail.com>
2025-07-08 21:44:05 +0300

    img

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 
 public class Main {
     public static void main(String[] args){
-
+        System.out.println("Test Not Found Book\n");
             Test.testNotFoundBook();
             System.out.println("/*****/");
+        System.out.println("Test Successful Payment\n");
             Test.testSuccessfulPayment();
             System.out.println("/*****/");
+        System.out.println("Test Out Of Stock\n");
             Test.testOutOfStock();
             System.out.println("/*****/");
+        System.out.println("Test Remove Out Dated Books");
             Test.testRemoveOutDatedBooks();
     }
 }
